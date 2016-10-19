d5beb6f44a7a0d369d805fcf49c2ba0ac17cae69
Dave Mayo <dave_mayo@harvard.edu>
2016-10-19 11:56:12 -0400

    Add fixes for audience/publish

--- backend/model/ead_converter.rb
+++ backend/model/ead_converter.rb
@@ -13,7 +13,7 @@ class HarvardEADConverter < EADConverter
       make :digital_object, {
         :digital_object_id => SecureRandom.uuid,
         :title => title,
-       } do |obj|
+      } do |obj|
          ancestor(:resource, :archival_object) do |ao|
           ao.instances.push({'instance_type' => 'digital_object', 'digital_object' => {'ref' => obj.uri}})
          end
@@ -44,6 +44,7 @@ class HarvardEADConverter < EADConverter
            # attrs on <daoloc>
            fv_attrs[:file_uri] = daoloc['xlink:href'] if daoloc['xlink:href']
            fv_attrs[:use_statement] = daoloc['xlink:role'] if daoloc['xlink:role']
+           fv_attrs[:publish] = daoloc['audience'] != 'internal'
 
            obj.file_versions << fv_attrs
          end
@@ -55,6 +56,101 @@ class HarvardEADConverter < EADConverter
       # nothing! this is here to override super's implementation to prevent duplicate daoloc processing
     end
   end # END configure
+
+  ### Publish/Audience fixes for various note fields and methods ###
+
+  def make_nested_note(note_name, tag)
+      content = tag.inner_text
+
+      make :note_multipart, {
+        :type => note_name,
+        :persistent_id => att('id'),
+		    :publish => att('audience') != 'internal',
+        :subnotes => {
+          'jsonmodel_type' => 'note_text',
+          'content' => format_content( content ),
+          'publish' => att('audience') != 'internal' # HAX:DAVE This is wrong, but only used in dimensions handling,
+        }
+      } do |note|
+        set ancestor(:resource, :archival_object), :notes, note
+      end
+  end
+
+
+  with 'bibliography' do
+    make :note_bibliography
+    set :persistent_id, att('id')
+    set :publish, att('audience') != 'internal'
+    set ancestor(:resource, :archival_object), :notes, proxy
+  end
+
+  with 'index' do
+    make :note_index
+    set :persistent_id, att('id')
+    set :publish, att('audience') != 'internal'
+    set ancestor(:resource, :archival_object), :notes, proxy
+  end
+
+  with 'chronlist' do
+    if  ancestor(:note_multipart)
+      left_overs = insert_into_subnotes
+    else
+      left_overs = nil
+      make :note_multipart, {
+             :type => node.name,
+             :persistent_id => att('id'),
+             :publish => att('audience') != 'internal'
+      } do |note|
+        set ancestor(:resource, :archival_object), :notes, note
+      end
+    end
+
+    make :note_chronology, {
+           :publish => att('audience') != 'internal'
+    } do |note|
+      set ancestor(:note_multipart), :subnotes, note
+    end
+
+    # and finally put the leftovers back in the list of subnotes...
+    if ( !left_overs.nil? && left_overs["content"] && left_overs["content"].length > 0 )
+      set ancestor(:note_multipart), :subnotes, left_overs
+    end
+  end
+
+  with 'dao' do
+    make :instance, {
+           :instance_type => 'digital_object'
+    } do |instance|
+      set ancestor(:resource, :archival_object), :instances, instance
+    end
+
+
+    make :digital_object, {
+           :digital_object_id => SecureRandom.uuid,
+           :publish => att('audience') != 'internal',
+           :title => att('title')
+         } do |obj|
+      obj.file_versions <<  {
+        :use_statement => att('role'),
+        :file_uri => att('href'),
+        :xlink_actuate_attribute => att('actuate'),
+        :xlink_show_attribute => att('show')
+      }
+      set ancestor(:instance), :digital_object, obj
+    end
+
+  end
+
+  with 'daodesc' do
+    make :note_digital_object, {
+           :type => 'note',
+           :persistent_id => att('id'),
+           :publish => att('audience') != 'internal',
+           :content => inner_xml.strip
+    } do |note|
+      set ancestor(:digital_object), :notes, note
+    end
+  end
 
 end # END class
 
